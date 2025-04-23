dd74ea92c338d443acc357f093bbeceea4d3b6af
siyun <siyun2072@naver.com>
2025-04-23 11:11:54 +0900

    feat : 폴더 구조 생성 및 db 연결

---
+++ be/sesiem/src/main/java/com/backend/sesiem/SesiemApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sesiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesiemApplication.class, args);
+	}
+
+}
