16a0efb94013c19e0f5d2b4eac9a058fd98c09b0
Baejihae <qowlgo00@gmail.com>
2025-05-03 01:25:24 +0900

    feat: Axios 요청/응답 인터셉터에 accessToken 자동 설정 및 refreshToken 재발급 처리 추가 [S12P31S109-217]

---
+++ fe/sesim/src/services/api.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const api = axios.create({
+  // TODO: 백엔드 이중화 구현 이후 baseURL 변경
+  baseURL: "http://k12s109.p.ssafy.io:8080/api",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      try {
+        const { data } = await api.post("/refresh", { "refreshToken": refreshToken });
+
+        localStorage.setItem("accessToken", data.accessToken);
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.clear();
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
