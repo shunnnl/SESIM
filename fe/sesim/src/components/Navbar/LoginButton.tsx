a76e68f08ca0d0e9d63e97eec5ec630c03adf1b1
Baejihae <qowlgo00@gmail.com>
2025-05-04 01:08:13 +0900

    refactor: 로그인 모달창 위치 변경 [S12P31S109-216]

--- fe/sesim/src/components/Navbar/LoginButton.tsx
+++ fe/sesim/src/components/Navbar/LoginButton.tsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
-import { LoginModal } from "../Popup/LoginModal";
-import { SignUpModal } from "../Popup/SignUpModal";
+interface LoginButtonProps {
+    setIsLoginModalOpen: () => void;
+}
 
-export const LoginButton = () => {
-    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-    const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
-
-    const handleSignUpClick = () => {
-        setIsLoginModalOpen(false);
-        setIsSignUpModalOpen(true);
-    };
-
-    const handleLoginClick = () => {
-        setIsLoginModalOpen(true);
-        setIsSignUpModalOpen(false);
-    };
-
+export const LoginButton: React.FC<LoginButtonProps> = ({ setIsLoginModalOpen }) => {
     return (
         <>
             <button 
                 className="text-white text-[18px] font-bold bg-gradient-to-r from-[#5EA3EC] to-[#6C72F4] rounded-[35px] px-[22px] py-[7px]"
-                onClick={() => setIsLoginModalOpen(true)}
+                onClick={() => setIsLoginModalOpen}
             >
                 로그인
             </button>
-            <LoginModal
-                isOpen={isLoginModalOpen}
-                onClose={() => setIsLoginModalOpen(false)}
-                onSwitchToSignUp={handleSignUpClick}
-            />
-            <SignUpModal
-                isOpen={isSignUpModalOpen}
-                onClose={() => setIsSignUpModalOpen(false)}
-                onSwitchToLogin={handleLoginClick}
-            />
         </>
     )
 }
