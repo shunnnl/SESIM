a261f97cc796833e4e048d19f79c36af2c7e1682
coliny8468 <141201769+coliny8468@users.noreply.github.com>
2025-05-05 00:53:37 +0900

    feat: 리스트아이템 정보 수정[S12P31D109-214]

--- fe/sesim/src/pages/KeyInfoPage.tsx
+++ fe/sesim/src/pages/KeyInfoPage.tsx
@@ -9,21 +9,24 @@ export const KeyInfoPage = () => {
                 modelName: "AuthGaurd",
                 ALBaddress: "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com",
                 APIKeyState: "DEPLOYED",
-                state: "DEPLOYED"
+                state: "DEPLOYED",
+                APIKey: "sjdlkjdsljd-adjskjlads12"
             },
             {
                 id: 2,
                 modelName: "DataWatch",
                 ALBaddress: "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com",
                 APIKeyState: "FAILED",
-                state: "FAILED"
+                state: "FAILED",
+                APIKey: "sjdlkjdsljd-adjskjlads12"
             },
             {
                 id: 3,
                 modelName: "WebSentinel",
                 ALBaddress: "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com",
                 APIKeyState: "DEPLOYING",
-                state: "DEPLOYING"
+                state: "DEPLOYING",
+                APIKey: "sjdlkjdsljd-adjskjlads12"
             }
         ],
         [
@@ -32,14 +35,16 @@ export const KeyInfoPage = () => {
                 modelName: "FileGuard",
                 ALBaddress: "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com",
                 APIKeyState: "DEPLOYED",
-                state: "DEPLOYED"
+                state: "DEPLOYED",
+                APIKey: "sjdlkjdsljd-adjskjlads12"
             },
             {
                 id: 5,
                 modelName: "CloudWatch",
                 ALBaddress: "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com",
                 APIKeyState: "FAILED",
-                state: "FAILED"
+                state: "FAILED",
+                APIKey: "sjdlkjdsljd-adjskjlads12"
             },
             {
                 id: 6,
