ecc9ea486fb8ab421b296c8c248b0178408ce6d4
shimkwkr <133209685+shimkwkr@users.noreply.github.com>
2025-05-04 23:40:48 +0900

    feat: 1,2 Step 구현 완료 [S12P31S109-163]

---
+++ fe/sesim/src/components/CreateProject/SecondStep.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { BigCard } from "./BigCard"
+import { FormStepHeader } from "./FormStepHeader"
+
+export const SecondStep = () => {
+    const [projectName, setProjectName] = useState('')
+    const [projectDescription, setProjectDescription] = useState('')
+
+    return (
+        <div className="mt-[120px]">
+            <FormStepHeader
+                step="02"
+                title="프로젝트 기본 정보" 
+                description="생성할 프로젝트의 이름과 설명을 입력해주세요."
+            />
+            <div className="mt-[15px]">
+                <BigCard>
+                    <div>
+                        <p className="text-[16px] font-bold">프로젝트 이름 <span className="text-[#FF7E7E]">*</span></p>
+                        <input 
+                            type="text" 
+                            className="mt-[10px] w-full bg-transparent border-[#D9D9D9] border-[2px] rounded-[10px] p-[10px] text-[16px] text-[#ffffff] placeholder:text-[#A3A3A3]" 
+                            placeholder="프로젝트 이름을 입력해주세요"
+                            value={projectName}
+                            onChange={(e) => setProjectName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mt-[15px]">
+                        <p className="text-[16px] font-bold">프로젝트 설명</p>
+                        <textarea 
+                            className="mt-[10px] w-full bg-transparent border-[#D9D9D9] border-[2px] rounded-[10px] p-[10px] text-[16px] text-[#ffffff] placeholder:text-[#A3A3A3] min-h-[200px] resize-y" 
+                            placeholder="프로젝트 설명을 입력해주세요"
+                            value={projectDescription}
+                            onChange={(e) => setProjectDescription(e.target.value)}
+                        />
+                    </div>
+
+                    <button className="mt-[10px] bg-[#2C304B] border-[#505671] border-[1px] rounded-[10px] p-[10px] flex flex-row items-center gap-[10px] h-[50px] hover:bg-[#3C4061] transition-colors duration-200 ml-auto">
+                        프로젝트 정보 저장
+                    </button>
+                </BigCard>
+            </div>
+        </div>
+    )
+}
--- fe/sesim/src/pages/CreateProjectPage.jsx
+++ fe/sesim/src/pages/CreateProjectPage.jsx
@@ -1,9 +1,7 @@
 import { PageTitleImageWithText } from "../components/common/PageTitleImageWithText";
 import backgroundImage from "../assets/images/create-project-bg.png";
-import { FormStepHeader } from "../components/CreateProject/FormStepHeader";
-import { BigCard } from "../components/CreateProject/BigCard";
-import cloudFormationIcon from "../assets/images/aws-cloudformation.png";
-
+import { FirstStep } from "../components/CreateProject/FirstStep";
+import { SecondStep } from "../components/CreateProject/SecondStep";
 export const CreateProjectPage = () => {
     return (
         <div>
@@ -14,30 +12,11 @@ export const CreateProjectPage = () => {
                 backgroundImage={backgroundImage}
             />
 
-            <div className="container-padding text-white py-[84px]">
-                <FormStepHeader
-                    step="01"
-                    title="IAM Role 연결" 
-                    description="SaaS 포털이 고객 AWS에 접근하려면 IAM Role 연결이 필요합니다."
-                    must={true}
-                />
-                <div className="mt-[15px]">
-                    <BigCard>
-                        <div>
-                            <p className="text-[16px] font-normal text-[#979797]">* CloudFormation 템플릿을 통해 손쉽게 Role을 생성할 수 있습니다.</p>
-                            <button className="mt-[10px] bg-[#2C304B] border-[#505671] border-[1px] rounded-[10px] p-[10px] flex flex-row items-center gap-[10px]">
-                                <img src={cloudFormationIcon} alt="cloudFormationIcon" className="w-[25px] h-[30px]" />
-                                <p className="text-[20px] font-bold">CloudFormation</p>
-                            </button>
-                        </div>
-                        <div>
-                            <p className="text-[16px] font-normal text-[#979797]">* 기존에 발급한 ARN이 있다면 아래에서 확인해 선택해 주세요.</p>
-                            
-                        </div>
-
-                        <p className="text-[16px] font-normal text-[#979797]">* 검증할 IAM Role의 ARN을 입력하고 Assume Role 권한을 확인합니다.</p>
-                    </BigCard>
-                </div>
+            <div className="container-padding text-white py-[120px]">
+                <FirstStep />
+                <SecondStep />
+                {/* <ThirdStep /> */}
+                {/* <FourthStep /> */}
             </div>
         </div>
     );
