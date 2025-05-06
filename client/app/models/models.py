4cfb4e1a58769eceae4976458793880dfc67a92f
박진훈 <g22203@naver.com>
2025-05-06 14:38:35 +0900

    feat: 테이블 id 자동증가 추가 및 생성 방법 수정 [S12P31S109-158]

--- client/app/models/models.py
+++ client/app/models/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, Float, ForeignKey, CHAR
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class DeploymentLog(Base):
     __tablename__ = "deployment_logs"
 
-    deployment_log_id = Column(Integer, primary_key=True)
+    deployment_log_id = Column(Integer, primary_key=True, autoincrement=True)
     model_id = Column(Integer, ForeignKey("models.model_id"), nullable=False)
     log_type = Column(String(20), nullable=False)
     status = Column(String(50), nullable=False)
@@ -15,26 +16,29 @@ class DeploymentLog(Base):
     logged_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime, nullable=False)
 
+
 class AITrain(Base):
     __tablename__ = "ai_trains"
 
-    ai_train_id = Column(Integer, primary_key=True)
+    ai_train_id = Column(Integer, primary_key=True, autoincrement=True)
     model_id = Column(Integer, ForeignKey("models.model_id"), nullable=False)
     path = Column(String(2000), nullable=False)
     created_at = Column(DateTime, nullable=False)
 
+
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
     nickname = Column(String(50), nullable=False)
     email = Column(String(100), nullable=False)
     created_at = Column(DateTime, nullable=False)
 
+
 class Model(Base):
     __tablename__ = "models"
 
-    model_id = Column(Integer, primary_key=True)
+    model_id = Column(Integer, primary_key=True, autoincrement=True)
     project_id = Column(Integer, ForeignKey("projects.project_id"), nullable=False)
     name = Column(String(255), nullable=False)
     api_key = Column(String(2000), nullable=False)
@@ -42,18 +46,20 @@ class Model(Base):
     dashboard = Column(Text, nullable=False)
     created_at = Column(DateTime, nullable=False)
 
+
 class Project(Base):
     __tablename__ = "projects"
 
-    project_id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
     name = Column(String(100), nullable=False)
     field = Column(String(255))
 
+
 class APILog(Base):
     __tablename__ = "api_logs"
 
-    api_log_id = Column(Integer, primary_key=True)
+    api_log_id = Column(Integer, primary_key=True, autoincrement=True)
     model_id = Column(Integer, ForeignKey("models.model_id"), nullable=False)
     name = Column(String(100), nullable=False)
     latency_ms = Column(Integer, nullable=False)
