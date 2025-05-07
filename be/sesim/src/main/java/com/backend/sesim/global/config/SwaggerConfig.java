6d73aa2ce50f759096fd4c7478a0e2eed3452f21
하시윤 <siyun2072@naver.com>
2025-05-07 13:20:50 +0900

    feat: cors 경로 수정[S12P31S109-212]

--- be/sesim/src/main/java/com/backend/sesim/global/config/SwaggerConfig.java
+++ be/sesim/src/main/java/com/backend/sesim/global/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -23,7 +26,13 @@ public class SwaggerConfig {
         // API 보안 요구사항 추가
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
+        // 서버 URL 설정 추가
+        Server productionServer = new Server()
+                .url("http://52.79.149.27")
+                .description("Production Server");
+
         return new OpenAPI()
+                .servers(Arrays.asList(productionServer))  // 서버 정보 추가
                 .components(new Components().addSecuritySchemes("bearerAuth", jwtScheme))
                 .security(java.util.List.of(securityRequirement))
                 .info(new Info()
