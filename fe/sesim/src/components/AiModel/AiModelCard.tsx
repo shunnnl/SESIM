e25bb1de4667b73e5864c7dc4198c1ca28af4777
shimkwkr <133209685+shimkwkr@users.noreply.github.com>
2025-05-08 02:17:13 +0900

    feat: AiModel 페이지 - 모든 모델리스트 API 연결  [S12P31S109-237]

--- fe/sesim/src/components/AiModel/AiModelCard.tsx
+++ fe/sesim/src/components/AiModel/AiModelCard.tsx
@@ -2,24 +2,24 @@ import { SmallAnimatedButton } from "./SmallAnimatedButton"
 import sesimImage from "../../assets/images/sesim-logo.png"
 
 interface AiModelCardProps {
-    modelName: string;
-    modelDescription: string;
-    modelDetailList: string[];
+    name: string;
+    featureTitle: string;
+    featureList: string[];
 }
 
-export const AiModelCard = ({ modelName, modelDescription, modelDetailList }: AiModelCardProps) => {
+export const AiModelCard = ({ name, featureTitle, featureList }: AiModelCardProps) => {
     return (
         <div className="bg-[#2C304B] rounded-[10px] border-[1px] border-[#505671] px-[50px] py-[40px] w-[320px] h-[310px] relative">
-            <p className="text-[12px] font-medium pl-[10px]">{modelDescription}</p>
+            <p className="text-[12px] font-medium pl-[10px]">{featureTitle}</p>
             <div className="flex items-center mt-[2px] mb-[10px]">
                 <img
                     src={sesimImage}
                     alt="global"
                     className="w-[40px] h-[40px]"
                 />
-                <p className="text-[25px] font-bold">{modelName}</p>
+                <p className="text-[25px] font-bold">{name}</p>
             </div>
-            {modelDetailList.map((detail, index) => (
+            {featureList.map((detail, index) => (
                 <p 
                     key={index}
                     className="text-[14px] font-medium pl-[10px] my-[8px]"
@@ -31,7 +31,7 @@ export const AiModelCard = ({ modelName, modelDescription, modelDetailList }: Ai
             <div className="absolute bottom-0 left-0 w-full px-[60px] pb-[40px]">
                 <SmallAnimatedButton
                     text="자세히 보기"
-                    link={`/ai-model/${modelName}`}
+                    link={`/ai-model/${name}`}
                     onClick={() => { window.scrollTo(0, 0); }}
                 />
             </div>
--- fe/sesim/src/components/AiModel/AllAiModels.tsx
+++ fe/sesim/src/components/AiModel/AllAiModels.tsx
@@ -1,41 +1,12 @@
 import { motion } from "framer-motion";
 import { AiModelCard } from "./AiModelCard";
 import globalImage from "../../assets/images/global.png";
-
-const allAiModels = [
-    {
-        modelName: "AuthGuard",
-        modelDescription: "비정상 로그인 패턴을 감지",
-        modelDetailList: ["로그인 시도", "위치 이상", "시간대 이상", "MFA 실패"]
-    },
-    {
-        modelName: "DataWatch",
-        modelDescription: "내부 데이터 조회 및 다운로드 이상행동 감지",
-        modelDetailList: ["민감 정보", "과도한 조회", "비정상 다운로드"]
-    },
-    {
-        modelName: "WebSentinel",
-        modelDescription: "웹 요청에서 해킹 시도와 접근 식별",
-        modelDetailList: ["URL 접근", "웹쉘", "SQL 인젝션"]
-    },
-    {
-        modelName: "NetPulse",
-        modelDescription: "이상 트래픽과 시스템 공격을 실시간 추적",
-        modelDetailList: ["DDos", "포트 스캔", "트래픽 급증"]
-    },
-    {
-        modelName: "EndPointEye",
-        modelDescription: "엔드포인트에서의 수상한 활동 탐지",
-        modelDetailList: ["프로세스 실행", "파일리스 공격", "권한 상승"]
-    },
-    {
-        modelName: "EndPointEye",
-        modelDescription: "엔드포인트에서의 수상한 활동 탐지",
-        modelDetailList: ["프로세스 실행", "파일리스 공격", "권한 상승"]
-    }
-]
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 export const AllAiModels = () => {
+    const { data } = useSelector((state: RootState) => state.aiModel);
+
     return (
         <motion.div 
             className="mt-[88px] mb-[300px] text-white"
@@ -55,17 +26,17 @@ export const AllAiModels = () => {
                         zIndex: 1
                     }}
                 ></div>
-                <img
-                    src={globalImage}
+                <img 
+                    src={globalImage} 
                     alt="global"
                     className="absolute top-1/2 left-1/2 w-[600px] h-[600px] -translate-x-1/2 -translate-y-1/2 object-cover z-10"
                 />
                 <div className="relative z-20 grid grid-cols-2 lg:grid-cols-3 gap-8">
-                    {allAiModels.map((model) => (
-                        <AiModelCard key={model.modelName} {...model} />
+                    {data.map((model) => (
+                        <AiModelCard key={model.id} {...model} />
                     ))}
                 </div>
             </div>
-        </motion.div>  
+        </motion.div>
     );
 };
