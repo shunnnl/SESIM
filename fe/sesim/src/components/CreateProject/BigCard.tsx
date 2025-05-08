9905fd31b870bd5d64cbc199f80564c427db9bd7
심근원 <shimkwkr@naver.com>
2025-05-08 15:05:12 +0900

    Revert "Merge branch 'feature/S12P31S109-237-API-AiModelPage-Jenkins' into 'main'"
    This reverts merge request !89

--- fe/sesim/src/components/CreateProject/BigCard.tsx
+++
@@ -1,7 +0,0 @@
-export const BigCard : React.FC<{children: React.ReactNode}> = ({ children }) => {
-    return (
-        <div className="bg-[#242C4D] rounded-[20px] border-[#505671] border-[1px] shadow-[0_0_30px_rgba(145,184,196,0.2)] px-[45px] py-[30px]">
-            {children}
-        </div>
-    );
-};
--- fe/sesim/src/components/common/BlueCircle.tsx
+++
@@ -1,5 +0,0 @@
-export const BlueCircle = () => {
-    return (
-        <div className="bg-[#196DFD] w-[10px] h-[10px] rounded-full"></div>
-    )
-}
