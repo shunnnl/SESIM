7a698d7a1f8b02862947479799edeeb50ec1b716
Baejihae <qowlgo00@gmail.com>
2025-05-09 22:45:47 +0900

    fix: sdk 다운로드 링크 연결 [S12P31S109-292]

--- fe/sesim/src/components/Navbar/SidebarMenu.tsx
+++ fe/sesim/src/components/Navbar/SidebarMenu.tsx
@@ -12,8 +12,7 @@ export const SidebarMenu: React.FC<SidebarMenuProps> = ({ onClickMenu }) => {
             <Link to="/about" onClick={onClickMenu}>소개</Link>
             <Link to="/ai-model" onClick={onClickMenu}>AI모델</Link>
             <Link to="/model-inference-service" onClick={onClickMenu}>모델추론 서비스</Link>
-            {/* TODO : SDK 다운로드 링크 연결 */}
-            <Link to="" onClick={onClickMenu}>SDK 다운로드</Link>
+            <Link to="/sdk-download" onClick={onClickMenu}>SDK 다운로드</Link>
             <Link to="/userinfo" onClick={onClickMenu}>마이페이지</Link>
         </div>
     );
