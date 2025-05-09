048d1d6a4a20104a4114951ff1e7faf337d0fd45
Baejihae <qowlgo00@gmail.com>
2025-05-09 15:42:17 +0900

    feat: Navbar 및 사용자 드롭다운 컴포넌트의 스타일 수정 및 사용자 정보 페이지 접근 로직 추가 [S12P31S109-292]

--- fe/sesim/src/components/Navbar/UserDropdown.tsx
+++ fe/sesim/src/components/Navbar/UserDropdown.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 interface UserDropdownProps {
   isOpen: boolean;
@@ -13,19 +13,25 @@ export const UserDropdown: React.FC<UserDropdownProps> = ({
   onLogout,
 }) => {
   const navigate = useNavigate();
-  
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   if (!isOpen) return null;
+
+  const InfoPage = ['/userinfo', '/apiusage', '/project', '/keyinfo'];
+  const isUserInfoPage = InfoPage.includes(currentPath);
 
   const menuItems = [
     { id: 'api', label: 'API 사용량', path: '/apiusage' },
     { id: 'projects', label: '프로젝트', path: '/project' },
     { id: 'keys', label: '키 발급', path: '/keyinfo' },
-  ];
+  ]
 
   const handleUserInfoClick = () => {
     navigate('/userinfo');
     onClose();
   };
+
 
   const handleMenuItemsClick = (path: string) => {
     navigate(path);
@@ -33,30 +39,36 @@ export const UserDropdown: React.FC<UserDropdownProps> = ({
   };
 
   return (
-    <div className="absolute right-0 top-1/2 translate-y-[60px] w-[180px] h-fit bg-[#07142B] rounded-[20px] shadow-[0px_0px_20px_#74D0F4] text-white font-['Pretendard'] z-50">
+    <div className="absolute right-0 top-1/2 translate-y-[60px] w-[180px] h-fit bg-[#07142B] rounded-[20px] shadow-[0px_0px_15px_rgba(116,208,244,0.4)] text-white font-['Pretendard'] z-50">
       <div className="flex flex-col items-start gap-[16px] my-[20px]">
-        <button 
-          className="font-semibold mx-[32px] text-[16px] hover:text-gray-400"
-          onClick={handleUserInfoClick}
-        >
-          회원 정보
-        </button>
-        <div className="w-full border-t border-[#3E4865]" />
-        <div className="flex flex-col items-start mx-[32px] py-[2px] gap-[22px]">
-          {menuItems.map(item => (
+
+        {!isUserInfoPage && (
+          <>
             <button
-              key={item.id}
-              className="font-medium text-white/80 text-[14px] self-stretch text-left hover:text-gray-400"
-              onClick={() => handleMenuItemsClick(item.path)}
+              className="font-semibold mx-[32px] text-[16px] hover:text-gray-400"
+              onClick={handleUserInfoClick}
             >
-              {item.label}
+              회원 정보
             </button>
-          ))}
-        </div>
-        <div className="w-full border-t border-[#3E4865]" />
+            <div className="w-full border-t border-[#3E4865]" />
+            <div className="flex flex-col items-start mx-[32px] py-[2px] gap-[22px]">
+              {menuItems.map(item => (
+                <button
+                  key={item.id}
+                  className="font-medium text-white/80 text-[14px] self-stretch text-left hover:text-gray-400"
+                  onClick={() => handleMenuItemsClick(item.path)}
+                >
+                  {item.label}
+                </button>
+              ))}
+            </div>
+            <div className="w-full border-t border-[#3E4865]" />
+          </>
+        )}
+
         <button
           onClick={onLogout}
-          className="font-semibold text-[16px] mx-[32px] hover:text-gray-300"
+          className="font-semibold text-[16px] mx-[32px] hover:text-gray-400"
         >
           로그아웃
         </button>
