65caec2dfc35827deed8abbb3cc3e996ae85057a
박진훈 <g22203@naver.com>
2025-05-09 21:46:35 +0900

    feat: 로그 저장을 비동기로 변경 [S12P31S109-243]

--- client/app/api/predict.py
+++ client/app/api/predict.py
@@ -2,8 +2,9 @@ from app.db.database import get_db
 from app.schemas.common import MessageResponse
 from app.schemas.predict.request import PredictRequest
 from app.schemas.predict.response import PredictResponse
-from app.services.predict_service import handle_prediction, handle_prediction_from_file
-from fastapi import APIRouter, Depends, Header, UploadFile, File, Form, BackgroundTasks
+from app.services.predict_service import handle_prediction, save_uploaded_file, handle_prediction_from_file_async
+from fastapi import APIRouter, Depends, Header, UploadFile, File, Form
+from fastapi import BackgroundTasks
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -18,12 +19,34 @@ def predict_api(
     return handle_prediction(request, db, api_key)
 
 
+# 동기 버전
+# @router.post("/v1/predict/file", response_model=MessageResponse)
+# def predict_file_api(
+#     model_id: int = Form(...),
+#     file: UploadFile = File(...),
+#     api_key: str = Header(..., alias="api-key"),
+#     db: Session = Depends(get_db)
+# ):
+#     handle_prediction_from_file(model_id, file, db, api_key)
+#     return {"message": "파일 예측 처리가 완료되었습니다."}
+
+
+# 비동기 버전
 @router.post("/v1/predict/file", response_model=MessageResponse)
 def predict_file_api(
-    model_id: int = Form(...),
-    file: UploadFile = File(...),
-    api_key: str = Header(..., alias="api-key"),
-    db: Session = Depends(get_db)
+        background_tasks: BackgroundTasks,
+        model_id: int = Form(...),
+        file: UploadFile = File(...),
+        api_key: str = Header(..., alias="api-key"),
+        db: Session = Depends(get_db)
 ):
-    handle_prediction_from_file(model_id, file, db, api_key)
-    return {"message": "파일 예측 처리가 완료되었습니다."}
+    # 1. 파일 저장
+    file_path, original_filename = save_uploaded_file(model_id, file)
+
+    # 2. 비동기 처리 등록
+    background_tasks.add_task(
+        handle_prediction_from_file_async,
+        model_id, original_filename, file_path, db, api_key
+    )
+
+    return {"message": "비동기 파일 예측 처리가 시작되었습니다."}
