56bb2fda15cba57b6a8cd76e9577a0f207a6279a
Baejihae <qowlgo00@gmail.com>
2025-05-09 13:33:29 +0900

    feat: authSlice를 스토어에 추가하여 인증 상태 관리 기능 통합 [S12P31S109-292]

--- fe/sesim/src/store/index.ts
+++ fe/sesim/src/store/index.ts
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import keyinfoReducer from "./keyinfoSlice"; 
 import projectReducer from "./projectSlice"; 
 import apiUsageReducer from "./APIUsageSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
     reducer: {
         keyinfo: keyinfoReducer, 
         project: projectReducer, 
         apiUsage: apiUsageReducer,
+        auth: authReducer,
     },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;  
+export type AppDispatch = typeof store.dispatch;
