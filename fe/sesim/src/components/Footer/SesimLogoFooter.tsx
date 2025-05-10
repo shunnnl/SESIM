67be9fffe2d0d3a3d5eaf35c9aea6e661440352f
심근원 <shimkwkr@naver.com>
2025-05-10 19:37:56 +0900

    fix: navbar, footer 로고 이미지화 [S12P31S109-254]

--- fe/sesim/src/components/Footer/SesimLogoFooter.tsx
+++ fe/sesim/src/components/Footer/SesimLogoFooter.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import sesimLogo from "../../assets/images/sesim-logo.png";
+import sesimLogo from "../../assets/images/sesim-footer-logo.webp";
 
 export const SesimLogoFooter: React.FC = () => {
     return (
         <div className="sesim-logo w-fit">
-            <Link to="/" className="flex items-center">
-                <img src={sesimLogo} alt="SESIM Logo" className="w-[64px] h-[64px]" />
-                <div className="flex flex-col items-end -space-y-[10px] text-white">
-                    <p className="text-[32px] font-bold">SESIM</p>
-                    <p className="text-[10px] font-medium">YOUR PARTNER</p>
-                </div>
+            <Link to="/">
+                <img 
+                    src={sesimLogo} 
+                    alt="SESIM Logo" 
+                    className="w-[100px] md:w-[120px] lg:w-[140px]"
+                />
             </Link>
         </div>
     );
