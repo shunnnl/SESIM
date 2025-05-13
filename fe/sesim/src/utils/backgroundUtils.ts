20579ddef8c5b730adf95693e0bf1f3f0587c981
배지해 <qowlgo00@naver.com>
2025-05-14 00:27:58 +0900

    design: 사용자 페이지에서의 배경색 변경 [S12P31S109-335]

--- fe/sesim/src/utils/backgroundUtils.ts
+++ fe/sesim/src/utils/backgroundUtils.ts
@@ -2,5 +2,10 @@ export const getPageBackgroundClass = (pathname: string): string => {
     if (pathname === "/") {
         return "bg-cyber-security bg-cover bg-center bg-no-repeat min-h-screen";
     }
+
+    if (pathname === "/apiusage" || pathname === "/project") {
+        return "bg-[#04101D] min-h-screen";
+    }
+
     return "bg-[#0A0C35] min-h-screen";
-}; 
+};
