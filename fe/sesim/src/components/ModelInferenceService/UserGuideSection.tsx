15546fe13af41f45909c928ec006205e39a77fd3
심근원 <shimkwkr@naver.com>
2025-05-15 09:57:40 +0900

    feat: 사용자 가이드 - STEP 03 구현 [S12P31S109-311]

--- fe/sesim/src/components/ModelInferenceService/UserGuideSection.tsx
+++ fe/sesim/src/components/ModelInferenceService/UserGuideSection.tsx
@@ -1,11 +1,13 @@
 import { UserGuideStep1 } from "./UserGuideStep1";
 import { UserGuideStep2 } from "./UserGuideStep2";
+import { UserGuideStep3 } from "./UserGuideStep3";
 
 export const UserGuideSection = () => {
     return (
         <div>
             <UserGuideStep1 />
             <UserGuideStep2 />
+            <UserGuideStep3 />
         </div>
     );
 };
