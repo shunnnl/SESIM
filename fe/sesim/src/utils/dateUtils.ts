1235e523066fbd93611fd4f54ba795630ee0674b
배지해 <qowlgo00@naver.com>
2025-05-17 16:36:52 +0900

    feat: 월의 시작 및 종료 날짜를 반환하는 유틸리티 함수 추가 - getMonthStartDate 및 getMonthEndDate 구현 [S12P31S109-335]

--- fe/sesim/src/utils/dateUtils.ts
+++ fe/sesim/src/utils/dateUtils.ts
@@ -15,3 +15,20 @@ export const getCurrentDate = (): string => {
 
   return new Date().toISOString().split('T')[0];
 };
+
+
+export const getMonthStartDate = (month: string): string => {
+  const date = new Date(month);
+  date.setDate(1);
+
+  return date.toISOString().split('T')[0];
+};
+
+
+export const getMonthEndDate = (month: string): string => {
+  const date = new Date(month);
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(0);
+
+  return date.toISOString().split('T')[0];
+};
