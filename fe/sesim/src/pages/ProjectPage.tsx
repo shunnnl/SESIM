28a69430667e96a5ac6a228d7b23c60d68370fba
배지해 <qowlgo00@naver.com>
2025-05-17 16:39:44 +0900

    refactor: 프로젝트 페이지에 분리한 탭바 컴포넌트 적용 [S12P31S109-335]

--- fe/sesim/src/pages/ProjectPage.tsx
+++ fe/sesim/src/pages/ProjectPage.tsx
@@ -2,40 +2,9 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { Project } from "../types/ProjectTypes";
+import { Tabbar } from "../components/common/Tabbar";
 import useDeploymentStateSSE from '../hooks/projectStateSSE';
 import KeyinfoItemList from "../components/ProjectPageComponents/ProjectListItem";
-import { LuChartColumnIncreasing } from "react-icons/lu";
-import { LuFolder } from "react-icons/lu";
-import { NavLink } from "react-router-dom";
-
-interface SidebarNavItemProps {
-    to: string;
-    icon: React.ElementType;
-    label: string;
-}
-
-const SidebarNavItem: React.FC<SidebarNavItemProps> = ({ to, icon: Icon, label }) => {
-    return (
-        <div>
-            <NavLink
-                to={to}
-                className={({ isActive }) =>
-                    `flex items-center gap-3 px-4 py-3 rounded-t-2xl transition duration-200 text-sm
-                    ${isActive ? "bg-[#04101D] font-semibold text-white" : "font-medium text-gray-400 hover:bg-[#04101D]"}`
-                }
-            >
-                {({ isActive }) => (
-                    <>
-                        <div className="flex-1 flex items-center gap-3">
-                            <Icon className={`text-xl ${isActive ? "text-white" : "text-gray-400"}`} />
-                            {label}
-                        </div>
-                    </>
-                )}
-            </NavLink>
-        </div>
-    );
-};
 
 export const ProjectPage = () => {
     useDeploymentStateSSE();
@@ -44,32 +13,7 @@ export const ProjectPage = () => {
 
     return (
         <div className="flex flex-col min-h-screen text-white">
-            {/* 탭바 */}
-            <div className="w-full bg-[#1D2433]">
-                <div className="flex flex-row container-padding content-end justify-between pt-2 pd-1">
-                    {/* 네비게이션 메뉴 */}
-                    <div className="flex flex-row items-center gap-5">
-                        <SidebarNavItem
-                            to="/apiusage"
-                            icon={LuChartColumnIncreasing}
-                            label="API 사용 대시보드"
-                        >
-                        </SidebarNavItem>
-                        <SidebarNavItem
-                            to="/project"
-                            icon={LuFolder}
-                            label="프로젝트"
-                        />
-                    </div>
-
-                    {/* 사용자 정보 */}
-                    <div className="flex flex-row items-center gap-3">
-                        <div className="text-sm font-medium text-gray-400">
-                            {localStorage.getItem("email")}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Tabbar />
 
             {/* 컨텐츠 영역 */}
             <div className="flex flex-col my-[44px] w-full container-padding">
